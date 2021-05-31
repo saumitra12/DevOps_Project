f67c918b5a14e97b48eb1e77bd0a1709a018c9ab
saumitra12 <78364475+saumitra12@users.noreply.github.com>
2021-05-31 12:39:32 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-const MONGODB_URL = "mongodb+srv://saumitra12:babu199876@cluster0.esp3o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const MONGODB_URL = "mongodb+srv://saumitra12:Babu199876@cluster0.esp3o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = 5000;
 
 module.exports = { MONGODB_URL, PORT };
